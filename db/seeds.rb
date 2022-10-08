8f566cee74272ac7287c732a5a7bcf5906f05662
Mickey <mickeyc11111@gmail.com>
2022-10-08 13:22:58 -0400

    clients table and seed data but seed id increasing

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@ Restaurant.create(name: "Joon Omakase")
 Restaurant.create(name: "The Alcove")
 Restaurant.create(name: "Antidote")
 
+Client.create(name: "Edward")
+Client.create(name: "John")
+Client.create(name: "Mel")
+
+
 puts "✅ Done seeding!"
