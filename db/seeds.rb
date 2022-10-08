26ff790cef6a92a2cde71939d4798333900628db
Mickey <mickeyc11111@gmail.com>
2022-10-08 13:16:42 -0400

    seed and db migrate for Restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "🌱 Seeding spices..."
-
 # Seed your database here
+Restaurant.create(name: "Joon Omakase")
+Restaurant.create(name: "The Alcove")
+Restaurant.create(name: "Antidote")
 
 puts "✅ Done seeding!"
